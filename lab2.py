afedeed1bfec4b3c9ac8b8be1cf597eef35a002c
kotoro777 <99185665+kotoro777@users.noreply.github.com>
2022-03-18 21:25:35 +0400

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,17 +1,15 @@
-f = open("numbers.txt", "r")
-
-text = f.read()
-numbers = []
-numbers = text.split()
-print('Исходные числа: ', numbers)
-
-print('Обработанные числа: ', end = '')
-for i in numbers:
-    if int(i) % 2 == 0:
-        _num = ''
-        for i2 in i:
-            if i2 in _num:
-                continue
+import time
+start = time.time()                 # запускаем таймер
+with open('numbers.txt', 'r') as f: # открываем файл
+    data = f.read().split()         # считываем каждый строчный символ в файле
+    s = ''
+    for i in data:
+        if int(i) % 2 == 0:         # находим четное число
+            if len(set(list(i))) == len(i): # проверяем нет ли в i повторяющихся цифр
+                s += i              #
             else:
-                _num+=i2
-        print(_num,  end = ', ')
+                s += ''.join(sorted(set(list(i)), key=i.index)) # убираем повторяющие цифры из числа
+            s += ' '                #
+    finish = time.time()
+    result = finish - start         # отключаем таймер
+    print(s, "Program time: " + str(result) + " seconds.") # выводим результат программы
