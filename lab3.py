ac911f33a1d9c506244d2f2e7d09dcde589fb011
kotoro777 <99185665+kotoro777@users.noreply.github.com>
2022-04-13 12:35:23 +0400

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -5,7 +5,7 @@ buffer_len = 1          # размер буфера чтения
 
 def task():
     try:
-        start = time.time()                   # запускаем таймер
+        start = time.time()  # запускаем таймер
         with open('text.txt', 'r') as file:   # открываем файл
             print("\n-----Результат работы программы-----\n")
 
@@ -15,9 +15,9 @@ def task():
                 number = ""  # переменная числа
 
                 while True:     # цикл для получения числа из посимволного чтения
-                    char = file.read(buffer_len)    # читаем очередной блок
+                    char = file.read(buffer_len)    # читаем блок
 
-                    if char == '':  # конец файла
+                    if char == '':  # проверяем конец файла
                         break
                     elif char == " ":
                         break
@@ -25,31 +25,34 @@ def task():
                         break
                     number += char          # складываем в переменную символ
 
-                if int(number) % 2 == 0:    # проверяем число четное
+                if number.isdigit():
 
-                    unique_num = list(set(list(number)))   # получаем список уникальных цифр числа
-                    new_num = ""                           # переменная новая число после удаления повторных цифр
+                    if int(number) % 2 == 0:    # проверяем число четное
 
-                    for n in number:    # проходимся по всем цифрам числа
+                        unique_num = list(set(list(number)))   # получаем список уникальных цифр числа
+                        new_num = ""                           # переменная новая число после удаления повторных цифр
 
-                        if n in unique_num:        # если цифра есть в списке уникальных цифр числа
+                        for n in number:    # проходимся по всем цифрам числа
 
-                            new_num += n           # добавляем цифру в новое число
-                            unique_num.remove(n)   # удалем из списка уникальных цифр числа
+                            if n in unique_num:        # если цифра есть в списке уникальных цифр числа
 
-                    print(new_num)
-                else:
-                    print(number)
+                                new_num += n           # добавляем цифру в новое число
+                                unique_num.remove(n)   # удалем из списка уникальных цифр числа
+
+                        print(new_num)
+                    else:
+                        print(number)
 
                 if char == '':  # проверка конец ли это файла
                     break
+
                 finish = time.time()
                 result = finish - start  # отключаем таймер
             print("Program time: " + str(result) + " seconds.")
 
     except FileNotFoundError:
         print("\nФайл text.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.")
-
+        
 def main():
     task()
 
